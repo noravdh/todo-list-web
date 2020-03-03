d1e70bbeaf3f6a8a70ef3f925cdead0b01588d6d
lindsey <lindsey@makers.tech>
2020-03-03 16:21:54 +0000

    Add feature tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,8 @@ require "capybara/rspec"
 feature "homepage" do
   scenario "I should be able to visit the homepage" do
     visit '/'
-    expect(page).to have_content 'Hello world'
+    expect(page).to have_content 'To do list'
+    expect(page).to have_content 'Add a todo list item'
+    expect(page).to have_content 'View to do list by category'
   end
 end
--- spec/features/todolist_spec.rb
+++ spec/features/todolist_spec.rb
@@ -2,6 +2,20 @@ require "capybara/rspec"
 
 feature "homepage" do
 
+  scenario "I should be able to see a list of to do items" do
+
+  end
+
   scenario "I should be able to add a todo item" do
   end
+
+  scenario "I should be able to see a list of items by category" do
+  end
+
+  scenario "I should be able to add an optional deadline to my todo item" do
+  end
+
+  scenario "I should be able to mark my to do item as complete" do
+  end
+
 end
